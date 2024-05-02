2955b57db6c861e685cc962079b9cb695a2415da
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2024-05-02 13:53:59 +0200

    lua: remove more rorr references

--- src/lua/lua_module.hpp
+++ src/lua/lua_module.hpp
@@ -20,15 +20,6 @@ namespace big
 		{
 			std::vector<sol::protected_function> m_on_all_mods_loaded_callbacks;
 
-			std::vector<sol::protected_function> m_pre_code_execute_callbacks;
-			std::vector<sol::protected_function> m_post_code_execute_callbacks;
-
-			std::unordered_map<void*, std::vector<sol::protected_function>> m_pre_builtin_execute_callbacks;
-			std::unordered_map<void*, std::vector<sol::protected_function>> m_post_builtin_execute_callbacks;
-
-			std::unordered_map<void*, std::vector<sol::protected_function>> m_pre_script_execute_callbacks;
-			std::unordered_map<void*, std::vector<sol::protected_function>> m_post_script_execute_callbacks;
-
 			std::vector<std::unique_ptr<lua::gui::gui_element>> m_menu_bar_callbacks;
 			std::vector<std::unique_ptr<lua::gui::gui_element>> m_always_draw_independent_gui;
 			std::vector<std::unique_ptr<lua::gui::gui_element>> m_independent_gui;
