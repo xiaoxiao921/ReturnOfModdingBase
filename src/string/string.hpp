471f4b9d7db8f3afe69bf081078a5d4e358e15ca
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-17 13:05:58 +0200

    string: add some utility functions

--- src/string/string.hpp
+++ src/string/string.hpp
@@ -22,15 +22,34 @@ namespace big::string
 	}
 
 	template<typename T = std::string>
-	inline std::vector<T> split(const std::string& text, const char delim)
+	inline std::vector<T> split(const std::string& text, const char delim, size_t max_splits = std::string::npos)
 	{
 		std::vector<T> result;
 		std::string str;
 		std::stringstream ss(text);
+		size_t splits = 0;
+
+		max_splits--;
+
 		while (std::getline(ss, str, delim))
 		{
-			result.push_back(get_text_value<T>(str));
+			if (splits < max_splits)
+			{
+				result.push_back(get_text_value<T>(str));
+				splits++;
+			}
+			else
+			{
+				std::string remaining;
+				if (std::getline(ss, remaining, '\0'))
+				{
+					str += delim + remaining;
+				}
+				result.push_back(get_text_value<T>(str));
+				break;
+			}
 		}
+
 		return result;
 	}
 
@@ -61,5 +80,52 @@ namespace big::string
 		}
 
 		return result;
+	}
+
+	inline std::string to_upper(const std::string& input)
+	{
+		std::string result = input;
+
+		// Use the std::locale to ensure proper case conversion for the current locale
+		std::locale loc;
+
+		for (size_t i = 0; i < result.length(); ++i)
+		{
+			result[i] = std::toupper(result[i], loc);
+		}
+
+		return result;
+	}
+
+	// trim from start (in place)
+	inline void ltrim(std::string& s)
+	{
+		s.erase(s.begin(),
+		        std::find_if(s.begin(),
+		                     s.end(),
+		                     [](unsigned char ch)
+		                     {
+			                     return !std::isspace(ch);
+		                     }));
+	}
+
+	// trim from end (in place)
+	inline void rtrim(std::string& s)
+	{
+		s.erase(std::find_if(s.rbegin(),
+		                     s.rend(),
+		                     [](unsigned char ch)
+		                     {
+			                     return !std::isspace(ch);
+		                     })
+		            .base(),
+		        s.end());
+	}
+
+	// trim from both ends (in place)
+	inline void trim(std::string& s)
+	{
+		rtrim(s);
+		ltrim(s);
 	}
 } // namespace big::string
