a03661816746d0a3fe4f10e11548ab09f45b1771
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-19 04:31:43 +0200

    lua config system: change parent table name to `config`. add doc

--- src/lua/bindings/toml_v2/toml_lua_v2.cpp
+++ src/lua/bindings/toml_v2/toml_lua_v2.cpp
@@ -56,17 +56,73 @@ namespace lua::toml_lua_v2
 
 	void bind(sol::table& state)
 	{
-		sol::table ns = state.create_named("toml_v2");
+		sol::table ns = state.create_named("config");
 
-		auto config_file_ut = ns.new_usertype<toml_v2::config_file>("config_file", sol::constructors<toml_v2::config_file(std::string_view, bool, std::string_view)>());
+		// Lua API: Class
+		// Name: config.config_file
+		// A helper class to handle persistent data.
 
+		// Lua API: Constructor
+		// Class: config.config_file
+		// Param: config_path: string: Full path to a file that contains settings. The file will be created as needed.
+		// Param: save_on_init: bool: If the config file/directory doesn't exist, create it immediately.
+		// Create a new config file at the specified config path.
+		auto config_file_ut = ns.new_usertype<toml_v2::config_file>("config_file",
+		                                                            sol::meta_function::construct,
+		                                                            sol::factories(
+		                                                                // dot syntax, no "self" value
+		                                                                // passed in
+		                                                                [](const std::string& config_path, bool save_on_init, sol::this_environment env)
+		                                                                {
+			                                                                return toml_v2::config_file(config_path, save_on_init, big::lua_module::guid_from(env));
+		                                                                },
+		                                                                // -- colon syntax, passes in the
+		                                                                // "self" value as first argument implicitly
+		                                                                [](sol::object, const std::string& config_path, bool save_on_init, sol::this_environment env)
+		                                                                {
+			                                                                return toml_v2::config_file(config_path, save_on_init, big::lua_module::guid_from(env));
+		                                                                }));
+
+		// Lua API: Function
+		// Class: config.config_file
+		// Name: bind
+		// Param: section: string: Section/category/group of the setting. Settings are grouped by this.
+		// Param: key: string: Name of the setting.
+		// Param: default_value: bool or number or string: Value of the setting if the setting was not created yet.
+		// Param: description: string: Simple description of the setting shown to the user.
+		// Returns: config_entry: new config_entry object.
+		// Create a new setting. The setting is saved to drive and loaded automatically.
+		// Each section and key pair can be used to add only one setting,
+		// trying to add a second setting will throw an exception.
 		config_file_ut.set_function("bind", sol::overload(bind_entry_bool, bind_entry_double, bind_entry_string));
+
+		// Lua API: Function
+		// Class: config.config_file
+		// Name: save
+		// Writes the config to disk.
 		config_file_ut.set_function("save", &toml_v2::config_file::save);
+
+		// Lua API: Function
+		// Class: config.config_file
+		// Name: reload
+		// Reloads the config from disk. Unsaved changes are lost.
 		config_file_ut.set_function("reload", &toml_v2::config_file::reload);
 
+		// Lua API: Class
+		// Name: config.config_entry
+		// Provides access to a single setting inside of a config_file.
 		auto config_entry_ut = ns.new_usertype<toml_v2::config_file::config_entry_base>("config_entry", sol::no_constructor);
 
+		// Lua API: Function
+		// Class: config.config_file
+		// Name: get
+		// Returns: val: bool or double or string. Value of this setting
 		config_entry_ut.set_function("get", get_value);
+
+		// Lua API: Function
+		// Class: config.config_file
+		// Name: set
+		// Param: new_value: bool or double or string: New value of this setting.
 		config_entry_ut.set_function("set", sol::overload(set_value_bool, set_value_double, set_value_string));
 	}
 } // namespace lua::toml_lua_v2
