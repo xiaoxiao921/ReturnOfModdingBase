54d82e37d38253dbe626309b93d397f796423295
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-24 19:21:58 +0200

    bump async logger

--- cmake_scripts/async-logger.cmake
+++ cmake_scripts/async-logger.cmake
@@ -4,7 +4,7 @@ message("AsyncLogger")
 FetchContent_Declare(
     AsyncLogger
     GIT_REPOSITORY https://github.com/xiaoxiao921/AsyncLogger.git
-    GIT_TAG 479d2385c2420a771499cc816f75e584632dc946
+    GIT_TAG 6a52a826cd5f615beed167aa097baa0533c122b6
 )
 FetchContent_MakeAvailable(AsyncLogger)
 
