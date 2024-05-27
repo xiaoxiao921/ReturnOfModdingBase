ff490a23a8aa2a7f5fb30130b837e8ad1b9934f5
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-27 20:24:18 +0200

    fix type mismatch

--- src/lua/bindings/gui.hpp
+++ src/lua/bindings/gui.hpp
@@ -3,7 +3,7 @@
 
 namespace lua::gui
 {
-	extern std::function<void()> g_on_is_open;
+	extern std::function<bool()> g_on_is_open;
 	extern std::function<void()> g_on_toggle;
 
 	void bind(sol::table& state);
