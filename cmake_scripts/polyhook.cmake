76701de97530076fad28fc0524f241f60c48a3d4
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-06-23 18:50:21 +0200

    bump polyhook

--- cmake_scripts/polyhook.cmake
+++ cmake_scripts/polyhook.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
 	polyhook
 	GIT_REPOSITORY https://github.com/stevemk14ebr/PolyHook_2_0.git
-	GIT_TAG fd2a88f09c8ae89440858fc52573656141013c7f
+	GIT_TAG 8cd6cb4ef0f2a599f35bdd46ac5833843aea6523
 )
 FetchContent_MakeAvailable(polyhook)
