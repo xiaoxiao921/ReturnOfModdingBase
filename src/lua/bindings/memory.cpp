92c9b4e11e3155937377a6a77645745e3b736fc7
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2026-05-10 22:09:36 +0200

    jitted function addr are now logged

--- src/lua/bindings/memory.cpp
+++ src/lua/bindings/memory.cpp
@@ -801,7 +801,7 @@ namespace lua::memory
 		code.relocateToBase((uintptr_t)jit_function_buffer.get());
 		code.copyFlattenedData(jit_function_buffer.get(), size);
 
-		LOG(DEBUG) << "JIT Stub: " << log.data();
+		LOG(DEBUG) << "JIT Stub: " << log.data() << " | " << jit_function_buffer.get();
 
 		lua_pushcfunction(lua_state, (lua_CFunction)jit_function_buffer.get());
 		lua_setglobal(lua_state, jitted_lua_func_global_name.c_str());
--- src/lua/bindings/runtime_func_t.cpp
+++ src/lua/bindings/runtime_func_t.cpp
@@ -287,7 +287,7 @@ namespace lua::memory
 		code.relocateToBase((uintptr_t)m_jit_function_buffer.data());
 		code.copyFlattenedData(m_jit_function_buffer.data(), size);
 
-		LOG(DEBUG) << "JIT Stub: " << log.data();
+		LOG(DEBUG) << "JIT Stub: " << log.data() << " | " << m_jit_function_buffer.data();
 
 		return (uintptr_t)m_jit_function_buffer.data();
 	}
@@ -600,7 +600,7 @@ namespace lua::memory
 		code.relocateToBase((uintptr_t)m_jit_function_buffer.data());
 		code.copyFlattenedData(m_jit_function_buffer.data(), size);
 
-		LOG(DEBUG) << "JIT Stub: " << log.data();
+		LOG(DEBUG) << "JIT Stub: " << log.data() << " | " << m_jit_function_buffer.data();
 
 		return (uintptr_t)m_jit_function_buffer.data();
 	}
