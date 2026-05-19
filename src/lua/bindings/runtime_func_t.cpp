87c51f808e7abfc8f2c76da9e053e04e024e0693
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2026-05-19 16:40:30 +0200

    fix compilation error wrong var name

--- src/lua/bindings/runtime_func_t.cpp
+++ src/lua/bindings/runtime_func_t.cpp
@@ -287,7 +287,7 @@ namespace lua::memory
 		code.relocateToBase((uintptr_t)m_jit_function_buffer.data());
 		code.copyFlattenedData(m_jit_function_buffer.data(), size);
 
-		LOG(DEBUG) << "JIT Stub: " << log.data() << " | " << (uintptr_t)jit_function_buffer.get() << " | " << size;
+		LOG(DEBUG) << "JIT Stub: " << log.data() << " | " << (uintptr_t)m_jit_function_buffer.data() << " | " << size;
 
 		return (uintptr_t)m_jit_function_buffer.data();
 	}
